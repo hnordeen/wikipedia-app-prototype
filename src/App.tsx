c49bdd731761ad1898f7d043df15d0a8cb64fad5
hnordeen <hnordeen@wikimedia.org>
2026-02-11 17:52:00 -0700

    Games hub and game

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
@@ -13,19 +13,30 @@ import { initiateDykPreload } from './services/preloadService';
 
 initiateDykPreload();
 
+const AppContent: React.FC = () => {
+  const location = useLocation();
+  const isLinkQuestRoute = location.pathname.startsWith('/games/linkquest');
+  
+  return (
+    <>
+      {!isLinkQuestRoute && <NavBar />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/games/linkquest" element={<LinkQuestPage />} />
+        <Route path="/games/linkquest/results" element={<LinkQuestResultsPage />} />
+        <Route path="/games/linkquest/explore" element={<LinkQuestExplorePage />} />
+        <Route path="/article/:title" element={<ArticlePage />} />
+      </Routes>
+    </>
+  );
+};
+
 const App: React.FC = () => (
   <Router basename="/wikipedia-app-prototype">
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/games" element={<GamesPage />} />
-      <Route path="/games/linkquest" element={<LinkQuestPage />} />
-      <Route path="/games/linkquest/results" element={<LinkQuestResultsPage />} />
-      <Route path="/games/linkquest/explore" element={<LinkQuestExplorePage />} />
-      <Route path="/article/:title" element={<ArticlePage />} />
-    </Routes>
+    <AppContent />
   </Router>
 );
 
